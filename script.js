eed537ec9fe78ed2c48110b57cfa4cde0dcef2bd
sagargourimath <43512261+sagargourimath@users.noreply.github.com>
2018-09-23 14:21:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    $(window).scroll(function(){
+        let position = $(this).scrollTop();
+        if(position >= 100) {
+            $('.nav-menu').addClass('custom-navbar');
+        } else {
+            $('.nav-menu').removeClass('custom-navbar');
+        }
+});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
